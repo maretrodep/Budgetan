626f844a297d23211897faeedcbf6a4122718678
Pedram Akbari <pedramakbari@Pedrams-MacBook-Pro.local>
2025-03-06 10:46:49 -0800

    Added model for expense

---
+++ backend/models/expense.py
@@ -0,0 +1,16 @@
+from backend import db
+from datetime import datetime
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    priority = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+    mood = db.Column(db.String(20), nullable=False)
+    tags = db.Column(db.String(100))
+    notes = db.Column(db.Text)
--- backend/models/user.py
+++ backend/models/user.py
@@ -9,6 +9,7 @@ class User(db.Model):
     profile_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
 
     def set_password(self, password):
         """Hash the password and store it."""
