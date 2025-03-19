82664d8accfe3787a47392e6dca814bee9f7300b
Pedram Akbari <pedramakbari@Pedrams-MacBook-Pro.local>
2025-03-19 08:42:55 -0700

    fixed incorrect expense and income value. it should have been time not timestamp

--- backend/routes/income.py
+++ backend/routes/income.py
@@ -102,7 +102,7 @@ def get_monthly_incomes():
         {
             "id": income.id,
             "amount": income.amount,
-            "time": income.times.isoformat(),
+            "time": income.time.isoformat(),
             "note": income.note if income.note else ""
         }
         for income in incomes
